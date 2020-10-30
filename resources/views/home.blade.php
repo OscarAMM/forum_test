631cd4cb5d6f8a99f2dcab6003fb99357ee1d1a6
Oscar Monsreal <A15216352@alumnos.uady.mx>
2020-10-30 09:07:28 -0600

    Jumbotron added at home file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 <div class="container">
+    <div class="jumbotron">
+        <h2 class="text-center">Sistema de foros</h2>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card">
+            <div class="card shadow-sm">
                 <div class="card-body">
                     <h4 class="card-title">Foro</h4>
                     <p class="small">Sección que listan los foros registrados en el sistema</p>
